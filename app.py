60105c2422ececbc5c4906952708aefa116ff267
VidhuVS19 <vidhu19122002@gmail.com>
2023-06-19 00:02:42 +0530

    name main added

--- app.py
+++ app.py
@@ -152,3 +152,6 @@ def home():
         q_terms = [term.lower() for term in query.strip().split()]
         results = calculate_sorted_order_of_documents(q_terms)[:20:]
     return render_template('index.html', form=form, results=results)
+
+if __name__ == "_main_":
+    app.run(debug=True)
